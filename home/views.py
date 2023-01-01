e40e940402f5fc8975aa468f02390676fb7f5a6a
Nathan Martinez <nathanemtz@gmail.com>
2023-01-01 15:39:29 -0800

    home app and error pages setup

---
+++ home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
+
+
+def error_400(request, exception=None):
+    return render(request, 'error_pages/400.html')
+
+
+def error_403(request, exception=None):
+    return render(request, 'error_pages/403.html')
+
+
+def error_404(request, exception=None):
+    return render(request, 'error_pages/404.html')
+
+
+def error_500(request, exception=None):
+    return render(request, 'error_pages/500.html')
