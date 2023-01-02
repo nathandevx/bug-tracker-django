e0da08213fa413e34be6d2e36af9aa9010d155d6
Nathan Martinez <nathanemtz@gmail.com>
2023-01-02 02:50:00 -0800

    timestampcreator model mixin

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TimestampCreatorMixin(models.Model):
+	creator = models.ForeignKey(verbose_name="Creator", to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='%(class)s_creator')
+	updater = models.ForeignKey(verbose_name="Updater", to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='%(class)s_updater')
+	created_at = models.DateTimeField(verbose_name="Date created", auto_now_add=True)
+	updated_at = models.DateTimeField(verbose_name="Date updated", auto_now=True)
+
+	class Meta:
+		abstract = True
+		verbose_name = "TimestampCreatorMixin"
+		verbose_name_plural = "TimestampCreatorMixin"
+
