1e8e7a07a506a6ada0532a559840eb520f4f2375
Nathan Martinez <nathanemtz@gmail.com>
2023-01-04 17:15:50 -0800

    tracker urls, views, templates

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,5 +3,9 @@ from tracker.views import general
 
 app_name = 'tracker'
 urlpatterns = [
-    path("dashboard/", general.Dashboard.as_view(), name="dashboard"),
+    path("dashboard/", general.Dashboard.as_view(), name='dashboard'),
+    path("all-tickets/", general.AllTicketListView.as_view(), name='all-tickets'),
+    path("resolved-tickets/", general.ResolvedTicketListView.as_view(), name='resolved-tickets'),
+    path("in-progress-tickets/", general.InProgressTicketListView.as_view(), name='in-progress-tickets'),
+    path("high-priority-tickets/", general.HighPriorityTicketListView.as_view(), name='high-priority-tickets'),
 ]
--- tracker/views/general.py
+++ tracker/views/general.py
@@ -1,9 +1,43 @@
-from django.views.generic import TemplateView
-from bug_tracker.constants import ALL_GROUPS
+from django.views.generic import TemplateView, ListView
+
+from bug_tracker.constants import ALL_GROUPS, PAG_BY
 from tracker.mixins import GroupsRequiredMixin
+from tracker.models import Ticket
 
 
 class Dashboard(GroupsRequiredMixin, TemplateView):
-	template_name = 'tracker/general/dashboard.html'
+	template_name = 'tracker/dashboard/dashboard.html'
 	groups = ALL_GROUPS
+
+
+class AllTicketListView(GroupsRequiredMixin, ListView):
+	model = Ticket
+	template_name = 'tracker/dashboard/all_tickets.html'
+	groups = ALL_GROUPS
+	paginate_by = PAG_BY
+	queryset = Ticket.objects.all()
+
+
+class ResolvedTicketListView(GroupsRequiredMixin, ListView):
+	model = Ticket
+	template_name = 'tracker/dashboard/resolved_tickets.html'
+	groups = ALL_GROUPS
+	paginate_by = PAG_BY
+	queryset = Ticket.objects.filter(status=Ticket.RESOLVED)
+
+
+class InProgressTicketListView(GroupsRequiredMixin, ListView):
+	model = Ticket
+	template_name = 'tracker/dashboard/in_progress_tickets.html'
+	groups = ALL_GROUPS
+	paginate_by = PAG_BY
+	queryset = Ticket.objects.filter(status=Ticket.IN_PROGRESS)
+
+
+class HighPriorityTicketListView(GroupsRequiredMixin, ListView):
+	model = Ticket
+	template_name = 'tracker/dashboard/in_progress_tickets.html'
+	groups = ALL_GROUPS
+	paginate_by = PAG_BY
+	queryset = Ticket.objects.filter(priority=Ticket.HIGH)
 
