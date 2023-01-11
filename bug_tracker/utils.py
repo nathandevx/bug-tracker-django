0041b09e365b047511311e339c69f5894ebdc31c
Nathan Martinez <nathanemtz@gmail.com>
2023-01-10 21:43:46 -0800

    get random phone # func

--- bug_tracker/utils.py
+++ bug_tracker/utils.py
@@ -3,6 +3,12 @@ import random
 from django.utils import timezone
 
 
+def get_random_phone_number():
+	number = str(random.randint(1000000000, 9999999999))
+	formatted_number = "{}-{}-{}".format(number[:3], number[3:6], number[6:])
+	return formatted_number
+
+
 def get_random_date(seconds: int = 31536000):
 	"""
 	2,628,288 = 30 days
