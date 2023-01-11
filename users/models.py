8a8131ec10c9ceb68fd45866807d1e1aa69de0d4
Nathan Martinez <nathanemtz@gmail.com>
2023-01-10 21:41:57 -0800

    add phone number field to users model and get users func

--- users/models.py
+++ users/models.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import AbstractUser
-from bug_tracker.constants import YEAR
+from django.db import models
+
+from bug_tracker.constants import YEAR, SUPERUSER_USERNAME
 
 
 class User(AbstractUser):
+	phone_number = models.CharField(max_length=20, default='000-000-0000')
 	@classmethod
 	def get_year_months_total_users(cls, year: int = YEAR):
 		"""
@@ -16,3 +19,12 @@ class User(AbstractUser):
 			total = cls.objects.filter(date_joined__year=year, date_joined__month=i + 1).count()
 			months_ticket_totals.append(total)
 		return months, months_ticket_totals
+
+	@classmethod
+	def get_all_users_not_superuser(cls):
+		"""
+		Gets all users but the superuser in random order.
+		It's in random order so you can sort them (like in the users table)
+		:return: queryset of users
+		"""
+		return cls.objects.exclude(username=SUPERUSER_USERNAME).order_by('?')
