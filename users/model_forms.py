d1f33e00e5f0de7b555581bec244730a20bd7469
Nathan Martinez <nathanemtz@gmail.com>
2023-01-13 09:23:47 -0800

    better way of setting password

--- users/model_forms.py
+++ users/model_forms.py
@@ -9,3 +9,11 @@ class UserModelForm(forms.ModelForm):
 		widgets = {
 			'password': forms.PasswordInput()
 		}
+
+	def save(self, commit=True):
+		user = super().save(commit=False)
+		user.set_password(self.cleaned_data["password"])
+		if commit:
+			user.save()
+		return user
+
